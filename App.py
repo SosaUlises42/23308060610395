f3d46c029135571a032644a3bbe88ff67d630eb8
SosaUlises42 <23308060610395@cetis61.edu.mx>
2025-11-03 21:12:17 -0900

    falta session y el html de Yepez

--- App.py
+++ App.py
@@ -7,13 +7,36 @@ app.secret_key = "clave-secreta"
 def index():
     return render_template('yepez.html')
 
+@app.route('/principal')
+def principal():
+    return render_template('index.html')
+
+@app.route('/buscar', methods = ['POST','GET'])
+def buscar():
+    if request.method == 'POST':
+        busqueda = request.form['busqueda']
+
+        if busqueda == "calendario" or busqueda == "agenda" or busqueda == "plan":
+            return redirect(url_for('calendary'))
+        elif busqueda == "control alimenticio" or busqueda == "contar calorias" or busqueda == "comparar alimentos":
+            flash('Estamos trabajando en esta seccion')
+            return redirect(url_for('principal'))
+        else:
+            flash('Ningun resultado coincide con la busqueda', 'error')
+            return redirect(url_for('principal')) 
+    
+    return render_template('index.html')
+
+@app.route('/calendary')
+def calendary():
+    return render_template('calendario.html')
+
 @app.route('/inciosecion')
 def sesion():
     return render_template('iniciosecion.html')
 
 @app.route('/registro', methods = ['POST','GET'])
 def registro():
-    error = None
     if request.method == 'POST':
         nombre = request.form['nombre']
         correo = request.form['correo']
@@ -24,10 +47,10 @@ def registro():
         
         if not nombre or not correo or not contraseña or not peso or not altura or not edad:
             flash("Todos los campos son obligatorios", "error")
-            redirect(url_for("index"))
+            return redirect(url_for("sesion"))
         else:
             flash(f"Nuevo usuario existente: nombre: {nombre} correo: {correo} contraseña: {contraseña} peso: {peso} altura: {altura} edad: {edad}")
-            redirect(url_for("index"))
+            return redirect(url_for("sesion"))
             
     return redirect(url_for('index'))
 
